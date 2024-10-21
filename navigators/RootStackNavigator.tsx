2abd8baa87099ac8ee976e6f3c804281984e3464
Andrew Browne <3808083+frankenwino@users.noreply.github.com>
2024-10-21 11:16:04 +0200

    add navigation between pages

--- navigators/RootStackNavigator.tsx
+++ navigators/RootStackNavigator.tsx
@@ -1,4 +1,6 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Pressable } from "react-native";
+import { Text } from "react-native-paper";
 import AddScreen from "../screens/AddScreen";
 import HomeScreen from "../screens/HomeScreen";
 
@@ -11,8 +13,18 @@ const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 export default function RootStackNavigator() {
   return (
-    <RootStack.Navigator initialRouteName="Add">
-      <RootStack.Screen name="Home" component={HomeScreen} />
+    <RootStack.Navigator initialRouteName="Home">
+      <RootStack.Screen
+        name="Home"
+        component={HomeScreen}
+        options={({ navigation }) => ({
+          headerRight: () => (
+            <Pressable onPress={() => navigation.navigate("Add")}>
+              <Text>+</Text>
+            </Pressable>
+          ),
+        })}
+      />
       <RootStack.Screen name="Add" component={AddScreen} />
     </RootStack.Navigator>
   );
